d87560dbfa1412e030b2a8ae6841a7e4a8674acf
evilqubit <simonstadros@gmail.com>
2014-03-26 10:34:47 +0200

    adding phantomjs file

--- test.js
+++ test.js
@@ -8,3 +8,9 @@ session.pingHost ("173.194.112.112", function (error, target) {
     else
         console.log (target + ": Alive");
 });
+
+
+mysql -u root -ptoor  < qordoba_store_db.sql
+
+
+product_id,model,sku,upc,location,quantity,stock_status_id,image,manufacturer_id,price,points,date_available,sort_order,status,date_added,date_modified,viewed,group_id,author_id,downloadable,reward
